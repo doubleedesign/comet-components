a14d5a04df24c99152eedb37223282f22c0dff21
Leesa Ward <leesa@doubleedesign.com.au>
2025-03-06 23:00:32 +1100

    Config: Don't ignore theme's Composer files because it's only used for autoloading, no dependencies

---
+++ packages/comet-canvas/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'doubleedesign/comet-canvas',
+        'pretty_version' => '0.0.1',
+        'version' => '0.0.1.0',
+        'reference' => null,
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'doubleedesign/comet-canvas' => array(
+            'pretty_version' => '0.0.1',
+            'version' => '0.0.1.0',
+            'reference' => null,
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
