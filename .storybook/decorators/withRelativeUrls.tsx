9ba6fc4a3cc23fbbfebc970b2b25586195c502f5
Leesa Ward <leesa@doubleedesign.com.au>
2025-04-18 12:45:01 +1000

    Deployment: Fix Storybook paths for prod; set up IDE config for Storybook deployment

--- .storybook/decorators/withRelativeUrls.tsx
+++ .storybook/decorators/withRelativeUrls.tsx
@@ -1,7 +1,12 @@
 // Decorator to add the server URL to the story context, so relative URLs can be used in the stories
 export const withRelativeUrls = (Story, context) => {
 	if (context.parameters.server && context.parameters.server.url) {
-		const baseUrl = process.env.BROWSER_TEST_URL || 'https://storybook.cometcomponents.io';
+		let baseUrl = 'https://cometcomponents.io';
+
+		// Check if we are in dev mode, because the build uses the env file so we falsely get the dev URL in the build if we use that to differentiate
+		if(window.location.hostname === 'storybook.comet-components.test') {
+			baseUrl = 'https://comet-components.test';
+		}
 
 		// Prepend baseUrl if it's a relative URL
 		if (!context.parameters.server.url.startsWith('http')) {
