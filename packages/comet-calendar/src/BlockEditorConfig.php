33a618d43fb60d55952e49a3592bd2760d184b74
Leesa Ward <leesa@doubleedesign.com.au>
2025-05-03 11:03:55 +1000

    Refactor/fix(WP): Styling for blocks in the editor

--- packages/comet-calendar/src/BlockEditorConfig.php
+++ packages/comet-calendar/src/BlockEditorConfig.php
@@ -2,9 +2,31 @@
 namespace Doubleedesign\Comet\WordPress\Calendar;
 
 class BlockEditorConfig {
+	private static array $theme_json;
+
 	function __construct() {
+		$this->load_merged_theme_json();
+
 		add_filter('allowed_block_types_all', [$this, 'event_allowed_block_types'], 30, 2);
 		add_action('init', [$this, 'register_page_template'], 20, 2);
+
+		if(is_admin()) {
+			add_action('init', [$this, 'register_component_stylesheets'], 20, 2);
+		}
+	}
+
+	protected function load_merged_theme_json(): void {
+		$plugin_theme_json_path = WP_PLUGIN_DIR . '/comet-plugin/src/theme.json';
+		$plugin_theme_json_data = json_decode(file_get_contents($plugin_theme_json_path), true);
+		$final_theme_json = $plugin_theme_json_data;
+
+		$theme_json_file = get_stylesheet_directory() . '/theme.json';
+		if(file_exists($theme_json_file)) {
+			$theme_json_data = json_decode(file_get_contents($theme_json_file), true);
+			$final_theme_json = self::array_merge_deep($plugin_theme_json_data, $theme_json_data);
+		}
+
+		self::$theme_json = $final_theme_json;
 	}
 
 	/**
@@ -16,6 +38,7 @@ class BlockEditorConfig {
 	function event_allowed_block_types($allowed_blocks, \WP_Block_Editor_Context $context = null): array {
 		if($context === null) return $allowed_blocks;
 		$post = $context->post;
+		if(!$post) return $allowed_blocks;
 
 		// If all blocks are allowed ($allowed_blocks = true), we need to get all registered blocks first
 		if($allowed_blocks === true) {
@@ -58,4 +81,50 @@ class BlockEditorConfig {
 		$post_type_object->template_lock = false;
 	}
 
+
+	/**
+	 * Register CSS that is loaded from the Comet Components plugin
+	 * This can then be used in block.json by the handle specified here, solving relative path and symlink incompatibilities
+	 */
+	function register_component_stylesheets(): void {
+		wp_register_style('comet-event-list-style', plugins_url('/comet-plugin/vendor/doubleedesign/comet-components-core/src/components/EventList/event-list.css'));
+		wp_register_style('comet-event-card-style', plugins_url('/comet-plugin/vendor/doubleedesign/comet-components-core/src/components/EventCard/event-card.css'));
+		wp_register_style('comet-date-block-style', plugins_url('/comet-plugin/vendor/doubleedesign/comet-components-core/src/components/DateBlock/date-block.css'));
+		wp_register_style('comet-date-range-block-style', plugins_url('/comet-plugin/vendor/doubleedesign/comet-components-core/src/components/DateRangeBlock/date-range-block.css'));
+	}
+
+	public static function hex_to_theme_color_name($hex): ?string {
+		$theme = self::$theme_json['settings']['color']['palette'];
+
+		return array_reduce($theme, function($carry, $item) use ($hex) {
+			if(isset($item['slug']) && isset($item['color'])) {
+				return strtoupper($item['color']) === strtoupper($hex) ? $item['slug'] : $carry;
+			}
+			return $carry;
+		}, null);
+	}
+
+	/**
+	 * Deep merging of a multidimensional array where one is a partial variation of the other
+	 * @param array $original
+	 * @param array $partial
+	 * @return array
+	 */
+	public static function array_merge_deep(array $original, array $partial): array {
+		$result = $original;
+
+		foreach($partial as $key => $value) {
+			// If both are arrays but the original is indexed (numeric keys),
+			// or if the value isn't an array, replace entirely
+			if(!isset($original[$key]) || !is_array($value) || (is_array($original[$key]) && array_is_list($original[$key]))) {
+				$result[$key] = $value;
+			}
+			// If both are associative arrays, merge recursively
+			else if(is_array($original[$key])) {
+				$result[$key] = self::array_merge_deep($original[$key], $value);
+			}
+		}
+
+		return $result;
+	}
 }
--- packages/comet-calendar/vendor/composer/autoload_classmap.php
+++ packages/comet-calendar/vendor/composer/autoload_classmap.php
@@ -9,6 +9,7 @@ return array(
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
     'Doubleedesign\\Comet\\WordPress\\Calendar\\Admin' => $baseDir . '/src/Admin.php',
     'Doubleedesign\\Comet\\WordPress\\Calendar\\BlockEditorConfig' => $baseDir . '/src/BlockEditorConfig.php',
+    'Doubleedesign\\Comet\\WordPress\\Calendar\\Blocks' => $baseDir . '/src/Blocks.php',
     'Doubleedesign\\Comet\\WordPress\\Calendar\\Events' => $baseDir . '/src/Events.php',
     'Doubleedesign\\Comet\\WordPress\\Calendar\\Fields' => $baseDir . '/src/Fields.php',
     'Doubleedesign\\Comet\\WordPress\\Calendar\\TemplateHandler' => $baseDir . '/src/TemplateHandler.php',
