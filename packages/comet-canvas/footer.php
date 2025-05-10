a4a4b6bb9abea8e38d3a4b78ece9d56aa6748c35
Leesa Ward <leesa@doubleedesign.com.au>
2025-05-10 10:56:46 +1000

    Fix(WP): Account for empty social links in footer

--- packages/comet-canvas/footer.php
+++ packages/comet-canvas/footer.php
@@ -9,12 +9,17 @@ use Doubleedesign\CometCanvas\NavMenus;
 $menuItems = NavMenus::get_simplified_nav_menu_items_by_location('footer');
 $menuComponent = new Menu(['context' => 'site-footer'], $menuItems);
 $socials = get_field('social_media_links', 'options');
-$iconLinksComponent = new IconLinks([
-	'aria-label' => 'Social media links',
-	'context'    => 'site-footer',
-], $socials);
+if($socials) {
+	$iconLinksComponent = new IconLinks([
+		'aria-label' => 'Social media links',
+		'context'    => 'site-footer',
+	], $socials);
+	$footerComponent = new SiteFooter(['backgroundColor' => 'dark'], [$iconLinksComponent, $menuComponent]);
+}
+else {
+	$footerComponent = new SiteFooter(['backgroundColor' => 'dark'], [$menuComponent]);
+}
 
-$footerComponent = new SiteFooter(['backgroundColor' => 'dark'], [$iconLinksComponent, $menuComponent]);
 $footerComponent->render();
 
 wp_footer(); ?>
