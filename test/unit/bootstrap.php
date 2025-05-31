8c49d8a7db1dd7f7dbec4a041641596df9d336f4
Leesa Ward <leesa@doubleedesign.com.au>
2025-05-31 22:38:21 +1000

    Test(Core): Add common mocks/setup to unit test bootstrap file

--- test/unit/bootstrap.php
+++ test/unit/bootstrap.php
@@ -1,4 +1,17 @@
 <?php
+use Doubleedesign\Comet\Core\Assets;
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../packages/core/vendor/autoload.php';
 require_once __DIR__ . '/../../vendor/antecedent/patchwork/Patchwork.php';
+
+beforeEach(function() {
+    $this->assetsMock = Mockery::mock('alias:' . Assets::class);
+    $this->assetsMock->shouldReceive('get_instance')->andReturnSelf();
+    $this->assetsMock->shouldReceive('add_stylesheet')->andReturnSelf();
+    $this->assetsMock->shouldReceive('add_script')->andReturnSelf();
+});
+
+afterEach(function() {
+    Mockery::close();
+});
