b25585893f004dd6ad494ba3b601d5abeb1ec24b
Leesa Ward <leesa@doubleedesign.com.au>
2025-09-26 16:11:34 +1000

    Refactor(Core): Streamline and centralise setting of context + BEM classes
    Doc updates to come; this may introduce breaking changes that need to be addressed in special cases

--- tests/unit/Pest.php
+++ tests/unit/Pest.php
@@ -1,2 +1,1 @@
 <?php
-uses()->in('../../packages/core/src');
--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../packages/core/vendor/autoload.php';
-require_once __DIR__ . '/../../vendor/antecedent/patchwork/Patchwork.php';
 require_once __DIR__ . '/../../vendor/phluent/phluent/src/Expect.php';
 require_once __DIR__ . '/../../vendor/phluent/phluent/src/Act.php';
