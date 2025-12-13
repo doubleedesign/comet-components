2d36ded85c63aef98b8d4b6aac428ff6541a8b33
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-13 17:43:00 +1100

    Refactor(WP Blocks): Start overhauling custom block setup
     incl. adding custom block editor controls where appropriate and ACF for content + rendering

--- packages/comet-canvas-blocks/header.php
+++ packages/comet-canvas-blocks/header.php
@@ -1,16 +1,15 @@
 <?php
-use Doubleedesign\Comet\Core\{Config, SiteHeader, Group, Menu};
+use Doubleedesign\Comet\Core\{Config, Group, Menu, SiteHeader};
 use Doubleedesign\CometCanvas\NavMenus;
 
 $globalBackground = Config::getInstance()->get('global_background')->value;
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?> lang="en">
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<title><?php wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 
--- packages/comet-canvas-blocks/src/ThemeStyle.php
+++ packages/comet-canvas-blocks/src/ThemeStyle.php
@@ -1,17 +1,13 @@
 <?php
 namespace Doubleedesign\CometCanvas;
 use Doubleedesign\Comet\Core\Config;
-use WP_Theme_JSON_Data;
 
 class ThemeStyle {
     protected string $embedded_css = '';
 
     public function __construct() {
-        add_action('init', [$this, 'set_css_variables_from_theme_json'], 20, 1);
         add_action('wp_head', [$this, 'add_css_variables_to_head'], 25);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_theme_stylesheets'], 20);
-
-        add_action('admin_init', [$this, 'set_css_variables_from_theme_json'], 20, 1);
         add_action('admin_head', [$this, 'add_css_variables_to_head'], 25);
 
         // Set defaults for components as per Config class in the core package
@@ -27,32 +23,28 @@ class ThemeStyle {
         }
     }
 
-    public function set_css_variables_from_theme_json(): void {
-        // Note: This needs to run after the same filter in the Comet Components plugin, or the theme object won't be correct
-        add_filter('wp_theme_json_data_theme', function(WP_Theme_JSON_Data|\WP_Theme_JSON_Data_Gutenberg $theme_json) {
-            $colours = $theme_json->get_data()['settings']['color']['palette']['theme'];
-            $gradients = $theme_json->get_data()['settings']['color']['gradients']; // TODO: Implement gradients here
-            $css = '';
+    private function get_css_variables_from_theme_json(): string {
+        $theme_json = \WP_Theme_JSON_Resolver::get_theme_data();
+        $colours = $theme_json->get_data()['settings']['color']['palette'];
+        // $gradients = $theme_json->get_data()['settings']['color']['gradients']; // TODO: Implement gradients here
+        $css = '';
 
-            if (isset($colours)) {
-                foreach ($colours as $colourData) {
-                    $css .= '--color-' . $colourData['slug'] . ': ' . $colourData['color'] . ";\n";
-                }
+        if (isset($colours)) {
+            foreach ($colours as $colourData) {
+                $css .= '--color-' . $colourData['slug'] . ': ' . $colourData['color'] . ";\n";
             }
+        }
 
-            $this->embedded_css = $css;
-
-            return $theme_json;
-        }, 20, 1);
+        return $css;
     }
 
     public function add_css_variables_to_head(): void {
-        echo '<style>:root {' . $this->embedded_css . '}</style>';
+        echo '<style>:root {' . $this->get_css_variables_from_theme_json() . '}</style>';
     }
 
     public function add_css_variables_to_block_editor(): void {
         // Attaching this to comet-global-styles ensures it overrides Comet's global.css
-        wp_add_inline_style('comet-global-styles', ":root { {$this->embedded_css} }");
+        wp_add_inline_style('comet-global-styles', ":root { {$this->get_css_variables_from_theme_json()} }");
     }
 
     public function enqueue_theme_stylesheets(): void {
@@ -62,7 +54,7 @@ class ThemeStyle {
 
         if (file_exists($parent)) {
             $parent = get_template_directory_uri() . '/style.css';
-            wp_enqueue_style('comet-canvas', $parent, $deps, '0.2.0'); // TODO: Get this dynamically
+            wp_enqueue_style('comet-canvas-blocks', $parent, $deps, '0.3.0'); // TODO: Get this dynamically
         }
 
         if (file_exists($child)) {
