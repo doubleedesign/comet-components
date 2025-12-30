d38ec0bcafb3eee60ce0addef30014da64ea091e
Leesa Ward <leesa@doubleedesign.com.au>
2025-12-30 14:49:21 +1100

    Refactor: Move a lot of TinyMCE custom stuff into a separate plugin; bring classic and blocks TinyMCE config more into line; version bump everything

--- packages/comet-canvas-classic/src/TinyMCEConfig.php
+++ packages/comet-canvas-classic/src/TinyMCEConfig.php
@@ -7,44 +7,14 @@ use Doubleedesign\Comet\Core\Config;
 class TinyMCEConfig {
 
     public function __construct() {
-        add_filter('tiny_mce_before_init', [$this, 'add_editor_styles'], 10, 1);
         add_filter('tiny_mce_before_init', [$this, 'add_theme_colours'], 10, 1);
 
         // Note: The Comet for ACF plugin also adds its own customisations to the buttons available in ACF WYSIWYG fields.
     }
 
     /**
-     * Load editor styles in ACF WYSIWYG fields
-     * Ref: https://pagegwood.com/web-development/custom-editor-stylesheets-advanced-custom-fields-wysiwyg/
-     *
-     * @param  $mce_init
-     *
-     * @wp-hook
-     *
-     * @return array
-     */
-    public function add_editor_styles($mce_init): array {
-        $content_css = '/editor-styles.css';
-        if (file_exists(get_stylesheet_directory() . $content_css)) {
-            $content_css = get_stylesheet_directory_uri() . $content_css . '?v=' . time(); // it caches hard, use this to force a refresh
-        }
-        else if (file_exists(get_template_directory() . $content_css)) {
-            $content_css = get_template_directory_uri() . $content_css . '?v=' . time();
-        }
-        else {
-            $content_css = false;
-        }
-
-        if (isset($mce_init['content_css']) && $content_css) {
-            $content_css_new = $mce_init['content_css'] . ',' . $content_css;
-            $mce_init['content_css'] = $content_css_new;
-        }
-
-        return $mce_init;
-    }
-
-    /**
-     * Add predefined colours
+     * Add predefined colours to the colour picker
+     * Note: This will probably be deprecated in favour of whole-component colour theming, as is done in the block editor version
      *
      * @wp-hook
      *
