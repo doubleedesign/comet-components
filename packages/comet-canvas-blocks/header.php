58c564fa8e2d62aa9795b579eb53a044bd3b47e9
Leesa Ward <leesa@doubleedesign.com.au>
2026-01-15 22:19:58 +1100

    Fix(WP Blocks theme): Fix where header gets menu from; animation loading smoothness

--- packages/comet-canvas-blocks/header.php
+++ packages/comet-canvas-blocks/header.php
@@ -1,5 +1,5 @@
 <?php
-use Doubleedesign\Comet\Core\{Config, Group, Menu, SiteHeader};
+use Doubleedesign\Comet\Core\{Config, Group, Menu, PreprocessedHTML, SiteHeader};
 use Doubleedesign\CometCanvas\NavMenus;
 
 $globalBackground = Config::getInstance()->get('global_background')->value;
@@ -10,10 +10,22 @@ $globalBackground = Config::getInstance()->get('global_background')->value;
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script type="text/javascript">
+		// Ensure body is made visible after a short delay to avoid flash of unstyled content (inline style sets it to 0 initially)
+		// and to avoid showing content before animations kick in (if they are present)
+		// This is done here in case anything goes wrong with other JS files
+		// - so the content will always become visible even if animation JS breaks for example
+		document.addEventListener('DOMContentLoaded', function() {
+			setTimeout(function () {
+				document.body.style.opacity = "1";
+			}, 100);
+		});
+	</script>
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class('frontend'); ?> data-global-background="<?php echo $globalBackground; ?>">
+<?php // Opacity 0 on initial load so we don't get a flash of content before the animations, if present?>
+<body <?php body_class('frontend'); ?> data-global-background="<?php echo $globalBackground; ?>" style="opacity:0">
 <?php wp_body_open(); ?>
 
 <?php
@@ -21,16 +33,44 @@ $menuItems = NavMenus::get_simplified_nav_menu_items_by_location('primary');
 $menuComponent = new Menu(['context' => 'site-header'], $menuItems);
 $logoId = get_option('options_logo');
 $logoUrl = wp_get_attachment_image_url($logoId, 'full');
-$headerComponent = new SiteHeader(
-    [
-        'logoUrl'         => $logoUrl,
-        'size'            => 'wide',
-        'breakpoint'      => '860px',
-        'responsiveStyle' => 'default',
-        'submenuIcon'     => 'fa-caret-down'
-    ],
-    [new Group(['context' => 'responsive'], [$menuComponent])]
-);
+
+$showContactDetails = apply_filters('comet_canvas_show_contact_details_in_header', false);
+if ($showContactDetails) {
+    ob_start();
+    get_template_part('template-parts/contact-details');
+    $contactBlockHtml = ob_get_clean();
+    $contactBlock = new PreprocessedHTML([], $contactBlockHtml);
+}
+
+if ($showContactDetails) {
+    $headerComponent = new SiteHeader(
+        [
+            'logoUrl'              => $logoUrl,
+            'size'                 => 'contained',
+            'breakpoint'           => '1024px',
+            'responsiveStyle'      => 'overlay',
+            'icon'                 => 'fa-bars',
+            'submenuIcon'          => 'fa-plus'
+        ],
+        [
+            new Group(['context' => 'site-header', 'shortName' => 'contact'], [$contactBlock]),
+            new Group(['context' => 'responsive'], [$contactBlock, $menuComponent])
+        ]
+    );
+}
+else {
+    $headerComponent = new SiteHeader(
+        [
+            'logoUrl'         => $logoUrl,
+            'size'            => 'wide',
+            'breakpoint'      => '860px',
+            'responsiveStyle' => 'default',
+            'submenuIcon'     => 'fa-caret-down'
+        ],
+        [new Group(['context' => 'responsive'], [$menuComponent])]
+    );
+}
+
 $headerComponent->render();
 ?>
 
