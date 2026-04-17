2f1ff156eb5cd46dc5b7cc63cb646ccc5a0b6e05
Leesa Ward <leesa@doubleedesign.com.au>
2026-04-17 11:49:39 +1000

    Feat(SiteHeader): Option to specify header and overlay background colours as attributes

--- packages/comet-canvas-blocks/header.php
+++ packages/comet-canvas-blocks/header.php
@@ -55,12 +55,14 @@ else {
 
 $headerComponent = new SiteHeader(
     [
-        'logoUrl'         => $logoUrl,
-        'size'            => apply_filters('comet_canvas_header_size', ContainerSize::DEFAULT),
-        'breakpoint'      => apply_filters('comet_canvas_header_breakpoint', '1024px'),
-        'responsiveStyle' => apply_filters('comet_canvas_header_responsive_style', 'overlay'),
-        'icon'            => apply_filters('comet_canvas_header_icon', 'fa-bars'),
-        'submenuIcon'     => apply_filters('comet_canvas_header_submenu_icon', 'fa-plus')
+        'logoUrl'                => $logoUrl,
+        'size'                   => apply_filters('comet_canvas_header_size', ContainerSize::DEFAULT),
+        'backgroundColor'        => apply_filters('comet_canvas_header_background_color', null),
+        'overlayBackgroundColor' => apply_filters('comet_canvas_header_overlay_background_color', null),
+        'breakpoint'             => apply_filters('comet_canvas_header_breakpoint', '1024px'),
+        'responsiveStyle'        => apply_filters('comet_canvas_header_responsive_style', 'overlay'),
+        'icon'                   => apply_filters('comet_canvas_header_icon', 'fa-bars'),
+        'submenuIcon'            => apply_filters('comet_canvas_header_submenu_icon', 'fa-plus')
     ],
     $content
 );
