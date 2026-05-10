7176376d5f33c4454064c692442251f56efb8c66
Leesa Ward <leesa@doubleedesign.com.au>
2026-05-10 23:22:27 +1000

    Refactor(EventList,CardList): Consolidate and simplify

--- packages/comet-calendar/src/blocks/upcoming-events/render.php
+++ packages/comet-calendar/src/blocks/upcoming-events/render.php
@@ -2,12 +2,11 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 /** @var array $block */
 
-use Doubleedesign\Comet\Core\{EventCard, EventList};
+use Doubleedesign\Comet\Core\{EventCard, EventList, Utils};
 use Doubleedesign\Comet\WordPress\Calendar\{TemplateHandler};
 
 // $inEditorContext = isset($block['mode']) && $block['mode'] === 'preview';
 $events = TemplateHandler::get_upcoming_event_ids($block['itemCount'] ?? 3);
-
 $cards = array_map(function($eventId) use ($block) {
     if (get_post_meta('sort_date', $eventId, true) !== '') { // Skip events without dates
         $title = get_the_title($eventId);
@@ -32,13 +31,11 @@ $filtered_cards = array_filter($cards, function($card) {
     return $card !== null;
 });
 
+$attributes = Utils::array_pick($block, ['colorTheme', 'size', 'hAlign', 'layout', 'maxPerRow', 'itemCount']);
+
 $component = new EventList([
-    'colorTheme'             => $block['colorTheme'] ?? 'primary',
-    'size'                   => $block['size'] ?? 'contained',
-    'hAlign'                 => $block['hAlign'] ?? 'start',
+	...$attributes,
     'heading'                => !empty($block['data']['heading']) ? $block['data']['heading'] : 'Upcoming Events',
-    'maxPerRow'              => $block['maxPerRow'] ?? 3,
-    'itemCount'              => $block['itemCount'] ?? 3,
     'viewAllUrl'             => get_post_type_archive_link('event')
 ], $filtered_cards);
 $component->render();
